e68336162157f930eea56d3e08ecb0af7271e319
316019385 <edtrelo18@ciencias.unam.mx>
2020-05-17 15:45:57 -0500

    Agregué dos métodos a la clase Auxiliar

--- Auxiliar/Auxiliar.java
+++ Auxiliar/Auxiliar.java
@@ -1,4 +1,18 @@
 package Auxiliar;
 
 public class Auxiliar {
+
+    //Método para imprimir arreglo char
+    public static void printArray(char [] arreglo){
+        for(int x = 0; x < arreglo.length; x++){
+            System.out.println(arreglo[x]);
+        }
+    }
+
+    //Método para imprimir arreglo char
+    public static void printArray(String [] arreglo){
+        for(int x = 0; x < arreglo.length; x++){
+            System.out.println(arreglo[x]);
+        }
+    }
 }
