bd19ffbfd3fb9bc2d41153cf597ca3ccd9c9a63b
AlexisRamos11 <jarp0712@gmail.com>
2020-05-16 22:51:37 -0500

    Lo que tengo que hacer

--- Cipher/Emisor.java
+++ Cipher/Emisor.java
@@ -7,6 +7,9 @@ public class Emisor extends SuperEnigma{
     private boolean available = false;
 
     //Por hacer el constructor
+    
+    //Espero me salga bien lo que me pide de los getters y el constructor ajjajaja
+    //Alexis hara el metodo CM, lo queesta en en esta clase y poner los de los signos.
     //Cambiar las vocales acentuadas
 
     //Sobreescritura del metodo de la superclase
