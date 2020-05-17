8e8d6effaa1a49b4adcc00e8c8876261509e48ba
316019385 <edtrelo18@ciencias.unam.mx>
2020-05-16 21:30:24 -0500

    Sobreescribí el método poli..no sé que

--- Cipher/Emisor.java
+++ Cipher/Emisor.java
@@ -6,11 +6,15 @@ public class Emisor extends SuperEnigma{
     private String privateKey;
     private boolean available = false;
 
-
     //Sobreescritura del metodo de la superclase
     public char[] transposition() {
         int clave = Integer.parseInt(this.privateKey);
         return super.transposition(clave, this.mensaje);
     }
 
+    public String [] cifradoDePolibioM(){
+        int clave = Integer.parseInt(this.privateKey);
+        return super.cifradoDePolibioM(clave, this.mensaje);
+    }
 }
+
