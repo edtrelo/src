50bfb10fde56fa11d3b61dded0fc16a43663d058
316019385 <edtrelo18@ciencias.unam.mx>
2020-05-24 11:52:29 -0500

    Correguí método cesarfall y cesar emoji fall

--- Comunication/Chat.java
+++ Comunication/Chat.java
@@ -10,35 +10,15 @@ public class Chat {
 
 
         Emisor em = new Emisor("1234");
-
         em.switchAvailable();
-
         em.setMensaje("Guadalajara");
 
-        String[] polibio = em.cifradoCesarEmoji(3);
-
-        String[] eneigma = em.enigma(2);
-
-        Auxiliar.printArray(eneigma);
-
-        Auxiliar.printArray(polibio);
+        String [] emoticones = em.cifradoCesarEmoji(8);
+        Auxiliar.printArray(emoticones);
 
         AllTuring al = new AllTuring();
 
-        String[] al22 = al.cesarEmojiFall(29,
-                "\uD83D\uDE08\uD83D\uDE17\uD83D\uDE02\uD83D\uDE05\uD83D" +
-                        "\uDE02\uD83D\uDE0D\uD83D\uDE02\uD83D\uDE0B\uD83D\uDE02\uD83D\uDE14\uD83D\uDE02");
-
-        Auxiliar.printArray(al22);
-
-        String[] gg = em.HASH();
-
-        Auxiliar.printArray(gg);
-
-        String [] roo = al.breakHASH(gg);
-
-        Auxiliar.printArray(roo);
-
+        String [] dec = al.cesarEmojiFall(35, emoticones);
 
     }
 
